b0e1dddb041ea2397f86fad4ce4410fa9d625c49
ByCode22 <yassin7476@gmail.com>
2024-12-15 11:59:35 +0100

    26th commit

--- src/Profile.js
+++ src/Profile.js
@@ -86,124 +86,131 @@ function Profile() {
   };
 
   return (
-    <div className="profile-container">
-      {/* Columna izquierda */}
-      <div className="profile-left">
-        <div className="profile-img-container">
-          <img src={profileImage} alt="Foto de perfil" className="profile-img-large" />
-          <input type="file" accept="image/*" onChange={handleImageChange} />
-        </div>
-        <button onClick={resetToDefault} className="reset-button">
-          Restablecer Imagen
-        </button>
-        <div className="profile-menu">
-          <button
-            className={`menu-button ${activeTab === 'datos' ? 'active' : ''}`}
-            onClick={() => setActiveTab('datos')}
-          >
-            Datos Personales
-          </button>
-          <button
-            className={`menu-button ${activeTab === 'password' ? 'active' : ''}`}
-            onClick={() => setActiveTab('password')}
-          >
-            Modificar Contraseña
-          </button>
-        </div>
-      </div>
-
-      {/* Columna derecha */}
-      <div className="profile-right">
-        {activeTab === 'datos' && (
-          <div className="account-info">
-            <h3>Información de la cuenta</h3>
-            {/* Datos del usuario */}
-            <>
-              <div className="info-item">
-                <span className="info-label">Nombre y Apellido:</span>
-                <span className="info-value">{name}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">DNI:</span>
-                <span className="info-value">{dni}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Edad:</span>
-                <span className="info-value">{age}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Sexo:</span>
-                <span className="info-value">{sex}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Correo Electrónico:</span>
-                <span className="info-value">{email}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Teléfono:</span>
-                <span className="info-value">{number}</span>
-              </div>
-            </>
-          </div>
-        )}
-
-        {activeTab === 'password' && (
-          <div className="account-info">
-            <h3>Cambiar Contraseña</h3>
-            <div className="info-item">
-              <input
-                type="password"
-                placeholder="Contraseña Actual"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                className="password-input"
-              />
-            </div>
-            <div className="info-item">
-              <input
-                type="password"
-                placeholder="Nueva Contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="password-input"
-              />
-            </div>
-            <div className="info-item">
-              <input
-                type="password"
-                placeholder="Confirmar Contraseña"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                className="password-input"
-              />
-            </div>
-            {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
-            {success && <p style={{ color: 'green', textAlign: 'center' }}>{success}</p>}
-            <button type="button" className="save-button">
-              Guardar Contraseña
-            </button>
-          </div>
-        )}
-      </div>
-
-      {/* Botón flotante */}
-      <button className="qr-button" onClick={toggleQRPopup}>
-        QR
+    <>
+      {/* Botón fuera del contenedor principal */}
+      <button className="back-home" onClick={() => window.location.href = 'http://localhost:3001/home'}>
+        Volver
       </button>
 
-      {/* Popup con el QR */}
-      {showQRPopup && (
-        <div className="qr-popup">
-          <div className="qr-popup-content">
-            <button className="close-popup" onClick={toggleQRPopup}>
-              ×
+      <div className="profile-container">
+        {/* Columna izquierda */}
+        <div className="profile-left">
+          <div className="profile-img-container">
+            <img src={profileImage} alt="Foto de perfil" className="profile-img-large" />
+            <input type="file" accept="image/*" onChange={handleImageChange} />
+          </div>
+          <button onClick={resetToDefault} className="reset-button">
+            Restablecer Imagen
+          </button>
+          <div className="profile-menu">
+            <button
+              className={`menu-button ${activeTab === 'datos' ? 'active' : ''}`}
+              onClick={() => setActiveTab('datos')}
+            >
+              Datos Personales
             </button>
-            <QRCodeCanvas value="http://localhost:3000/resultados" size={200} />
-            <p>Escanee el código QR para ver los resultados.</p>
+            <button
+              className={`menu-button ${activeTab === 'password' ? 'active' : ''}`}
+              onClick={() => setActiveTab('password')}
+            >
+              Modificar Contraseña
+            </button>
           </div>
         </div>
-      )}
-    </div>
+
+        {/* Columna derecha */}
+        <div className="profile-right">
+          {activeTab === 'datos' && (
+            <div className="account-info">
+              <h3>Información de la cuenta</h3>
+              {/* Datos del usuario */}
+              <>
+                <div className="info-item">
+                  <span className="info-label">Nombre y Apellido:</span>
+                  <span className="info-value">{name}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">DNI:</span>
+                  <span className="info-value">{dni}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">Edad:</span>
+                  <span className="info-value">{age}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">Sexo:</span>
+                  <span className="info-value">{sex}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">Correo Electrónico:</span>
+                  <span className="info-value">{email}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">Teléfono:</span>
+                  <span className="info-value">{number}</span>
+                </div>
+              </>
+            </div>
+          )}
+
+          {activeTab === 'password' && (
+            <div className="account-info">
+              <h3>Cambiar Contraseña</h3>
+              <div className="info-item">
+                <input
+                  type="password"
+                  placeholder="Contraseña Actual"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  className="password-input"
+                />
+              </div>
+              <div className="info-item">
+                <input
+                  type="password"
+                  placeholder="Nueva Contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="password-input"
+                />
+              </div>
+              <div className="info-item">
+                <input
+                  type="password"
+                  placeholder="Confirmar Contraseña"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="password-input"
+                />
+              </div>
+              {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
+              {success && <p style={{ color: 'green', textAlign: 'center' }}>{success}</p>}
+              <button type="button" className="save-button">
+                Guardar Contraseña
+              </button>
+            </div>
+          )}
+        </div>
+
+        {/* Botón flotante */}
+        <button className="qr-button" onClick={toggleQRPopup}>
+          QR
+        </button>
+
+        {/* Popup con el QR */}
+        {showQRPopup && (
+          <div className="qr-popup">
+            <div className="qr-popup-content">
+              <button className="close-popup" onClick={toggleQRPopup}>
+                ×
+              </button>
+              <QRCodeCanvas value="http://localhost:3000/resultados" size={200} />
+              <p>Escanee el código QR para ver los resultados.</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
